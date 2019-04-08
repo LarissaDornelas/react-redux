42fa84554351a95c1788365460ed3f0258f3229f
LarissaDornelas <larissa.e.dornelas@gmail.com>
2019-04-08 11:02:15 -0300

    exercícios React - Aulas 18 a 28

---
+++ exercicios_react/ex.6/silvaFamily.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='João' lastName='Silva' />
+        <Member name='Maria' lastName='Silva' />
+        <Member name='Carla' lastName='Silva' />
+        <Member name='Paulo' lastName='Silva' />
+        <Member name='Mario' lastName='Silva' />
+
+
+    </div>
+)
